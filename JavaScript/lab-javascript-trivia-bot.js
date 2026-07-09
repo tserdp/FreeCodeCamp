1667db5cb61eaf37023ec4964cd5520d021d968d
RDP <RDP@RDP01.skyline.local>
2026-07-09 13:03:27 -0400

    Initial commit

---
+++ JavaScript/lab-javascript-trivia-bot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "TriviaBot";
+let botLocation = "the internet";
+let favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `Did you know that ${favoriteLanguage} was created in just 10 days by Brendan Eich in 1995?`;
+console.log(codingFact);
+
+codingFact = `Another fun fact: ${favoriteLanguage} is used by over 95% of all websites!`;
+console.log(codingFact);
+
+codingFact = `And here's one more: ${favoriteLanguage} can be used for both front-end and back-end development!`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
