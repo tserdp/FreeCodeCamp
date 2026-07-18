4d6c3a5707876ba4d64d3b3236e9c53cb96f373c
RDP <RDP@RDP01.skyline.local>
2026-07-18 19:47:45 -0400

    Add playground and sentence maker scripts

---
+++ JavaScript/00-playground.js
@@ -0,0 +1,5 @@
+let str = "hello";
+console.log(typeof str); // "string"
+
+let num = 42;
+console.log(typeof num); // "number"
---
+++ JavaScript/lab-sentence-maker.js
@@ -0,0 +1,26 @@
+let adjective, noun, verb, place, adjective2, noun2;
+
+adjective = "beautiful";
+noun = "dog";
+verb = "runs";
+place = "park";
+adjective2 = "happy";
+noun2 = "child";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "lazy";
+noun = "cat";
+verb = "sleeps";
+place = "house";
+adjective2 = "grumpy";
+noun2 = "mouse";
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
