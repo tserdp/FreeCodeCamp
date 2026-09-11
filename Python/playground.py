e036ac130d105a0910690a42438ca72abc4c7278
Rene De Posada <rene.de.posada@thestarfishedge.com>
2026-09-11 15:37:03 +0000

    feat: add playground script and workshop report card printer

--- Python/playground.py
+++ Python/playground.py
@@ -1,1 +1,7 @@
 print('My favorite programming languages are', 'Python', 'JavaScript', 'and', 'React')
+
+my_integer_var = 10
+print(type(my_integer_var))  # <class 'int'>
+
+account_balance = '12'
+print(isinstance(account_balance, int)) # False
