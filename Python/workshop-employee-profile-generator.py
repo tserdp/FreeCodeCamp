f11441ca9fce4cb203870c14852c81690230b8b4
Rene De Posada <rene.de.posada@thestarfishedge.com>
2026-09-15 19:39:43 +0000

    feat: refactor employee profile generator script to improve structure and output

--- Python/workshop-employee-profile-generator.py
+++ Python/workshop-employee-profile-generator.py
@@ -1,7 +1,22 @@
 first_name = "John"
 last_name = "Doe"
-print(first_name)
-print(last_name)
+full_name = first_name + ' ' + last_name
 
-full_name = first_name + last_name
-print(full_name)
+address = "123 Main Street"
+address += ", Apartment 4B"
+
+employee_age = 28
+
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+
+experience_years = 5
+experience_info = 'Experience: ' +str(experience_years) + ' years'
+print(experience_info)
+
+position = 'Data Analyst'
+
+salary = 75000
+
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
